00c0decae488b7c0a8141e62351032893622b6a9
Ignacio <rogerpeke97@gmail.com>
2020-09-14 01:51:35 -0300

    Updates

---
+++ precache-manifest.c71909eb2b7717c5644c0cb74ea3e946.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1572c52c6749772d4a43d14c5ce9671f",
+    "url": "/EduwebReact/index.html"
+  },
+  {
+    "revision": "20e1e9cbe884180e330c",
+    "url": "/EduwebReact/static/css/main.f26aea4e.chunk.css"
+  },
+  {
+    "revision": "c57cfc6d176c82aed860",
+    "url": "/EduwebReact/static/js/2.d46c52c4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/EduwebReact/static/js/2.d46c52c4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "20e1e9cbe884180e330c",
+    "url": "/EduwebReact/static/js/main.f8687499.chunk.js"
+  },
+  {
+    "revision": "77b4eb18dd8a4986d496",
+    "url": "/EduwebReact/static/js/runtime-main.60d61582.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/EduwebReact/static/media/logo.5d5d9eef.svg"
+  }
+]);
