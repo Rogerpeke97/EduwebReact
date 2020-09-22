3e81884ce5674b69d4866e7e91eae79c99e0eef4
Ignacio <rogerpeke97@gmail.com>
2020-09-22 02:12:04 -0300

    Updates

---
+++ precache-manifest.848727beb910d29798600fd4e8f06f9a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bfa0fe90bb91bf78d6050b223b3e1b41",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b0002b1a20c8e7a1e3b1",
+    "url": "/static/css/main.5c183e3a.chunk.css"
+  },
+  {
+    "revision": "a5d6fa19fda7d69dbc0e",
+    "url": "/static/js/2.97b552a8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.97b552a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b0002b1a20c8e7a1e3b1",
+    "url": "/static/js/main.6996096b.chunk.js"
+  },
+  {
+    "revision": "c98facc933c51d4aa07d",
+    "url": "/static/js/runtime-main.4c163770.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/static/media/logo.ee7cd8ed.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/EduwebReact/precache-manifest.cf0e9d8d248fc0229f41ec550257b607.js"
+  "/precache-manifest.848727beb910d29798600fd4e8f06f9a.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/EduwebReact/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
